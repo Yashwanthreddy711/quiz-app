639c203507924f1dfd3d1be5f1925768b67d299b
yashwanth <yashwanthreddy7675@gmail.com>
2024-05-21 12:25:28 +0530

    fetch the quiz data from the api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import axios from "axios";
+import { React,useEffect,useState } from "react";
 
 function App() {
+  
+const [data,setData]=useState([]);
+
+const fetchData=async()=>{
+
+    try{
+
+       const response=await axios.get('https://opentdb.com/api.php?amount=30&category=18&difficulty=medium&type=multiple')
+       setData(response.data.results);
+
+    }
+    catch(error){
+      console.error(error.message);
+    }
+
+
+}
+console.log(data);
+useEffect(()=>{
+  fetchData();
+});
   return (
-    <div className="App">
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">    
+    <h1>Fetching Api data</h1> 
     </div>
   );
 }
